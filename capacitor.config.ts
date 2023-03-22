6f54a2a95dcba73064698c2506fe684108cc3cff
Viacheslav Andreev <maxivimax@icloud.com>
2023-03-22 16:07:16 +0300

    Some changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.maxivimax.LineTodo',
   appName: 'LineTodo',
   webDir: 'dist',
   bundledWebRuntime: false
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,24 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <div id="container"
+        class="w-screen h-screen py-[10vw] px-[10vh] text-center flex flex-col items-center justify-between">
+        <div>
+          <div class="text-[10vw] font-black">Дата</div>
+          <div class="dates w-screen flex flex-row font-bold text-[20vw] overflow-x-scroll">
+            <span class="dateItem">1</span>
+            <span class="dateItem">2</span>
+            <span class="dateItem">3</span>
+            <span class="dateItem">4</span>
+            <span class="dateItem">5</span>
+            <span class="dateItem">6</span>
+            <span class="dateItem">7</span>
+            <span class="dateItem">8</span>
+            <span class="dateItem">9</span>
+            <span class="dateItem">10</span>
+          </div>
+          <div class="text-[5vw] font-black">Марта</div>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -26,31 +29,12 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.dateItem {
+  margin-left: 1vh;
+  margin-right: 1vh;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.dates::-webkit-scrollbar {
+  display: none;
 }
 </style>
